578fe45212a31e54d8cdefc41b2c9778500d58a5
Sanya <sanya2371@gmail.com>
2017-09-24 19:02:46 +0300

    Add project files.

---
+++ AkavacheTests/AkavacheTests/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AkavacheTests.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
